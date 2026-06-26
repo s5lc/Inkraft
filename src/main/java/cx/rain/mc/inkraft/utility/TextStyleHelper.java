be755082f43b74899806db9d6f090fcb40dfa62b
雨落 <53731501+qyl27@users.noreply.github.com>
2026-06-26 12:51:19 +0800

    Use LegacyComponentSerializer as deserialize fallback. (#2)
    Signed-off-by: 秋雨落 <i@rain.cx>

--- src/main/java/cx/rain/mc/inkraft/utility/TextStyleHelper.java
+++ src/main/java/cx/rain/mc/inkraft/utility/TextStyleHelper.java
@@ -4,6 +4,7 @@ import net.kyori.adventure.platform.modcommon.MinecraftAudiences;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.minimessage.ParsingException;
 import net.kyori.adventure.text.minimessage.tag.standard.StandardTags;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.network.chat.Component;
 import org.slf4j.Logger;
@@ -12,25 +13,35 @@ import org.slf4j.LoggerFactory;
 public class TextStyleHelper {
     private static final Logger log = LoggerFactory.getLogger(TextStyleHelper.class);
     private static final MiniMessage MINI_MESSAGE = MiniMessage.builder()
-            .tags(StandardTags.defaults())
-            .strict(false)
-            .build();
+        .tags(StandardTags.defaults())
+        .strict(false)
+        .build();
+    private static final LegacyComponentSerializer LEGACY_AMPERSAND = LegacyComponentSerializer.builder()
+        .character(LegacyComponentSerializer.AMPERSAND_CHAR)
+        .hexColors()
+        .hexCharacter('H')
+        .build();
+    private static final LegacyComponentSerializer LEGACY_SECTION = LegacyComponentSerializer.builder()
+        .extractUrls()
+        .hexColors()
+        .useUnusualXRepeatedCharacterHexFormat()
+        .build();
 
-    public static Component parseStyle(String literalText, HolderLookup.Provider registries) {
+    public static Component parseStyle(String text, HolderLookup.Provider registries) {
         try {
-            var adventureComponent = MINI_MESSAGE.deserialize(literalText);
-            return MinecraftAudiences.nonWrappingSerializer(() -> registries).serialize(adventureComponent);
+            var component = MINI_MESSAGE.deserialize(text);
+            return MinecraftAudiences.nonWrappingSerializer(() -> registries).serialize(component);
         } catch (RuntimeException ex) {
             if (ex instanceof ParsingException pex) {
                 var detailMessage = pex.detailMessage();
                 if (detailMessage != null && detailMessage.contains("Legacy formatting codes")) {
                     // Legacy formatting code (§) is not supported by minimessage. Fallback silently.
-                    // There's no adventure-text-serializer-legacy in adventure-platform-mod-shared, or we can make LegacyComponentSerializer as fallback.
-                    return Component.literal(literalText);
+                    var component = LEGACY_SECTION.deserialize(text);
+                    return MinecraftAudiences.nonWrappingSerializer(() -> registries).serialize(component);
                 }
             }
-            log.warn("Failed to parse MiniMessage text: {}", literalText, ex);
-            return Component.literal(literalText);
+            log.warn("Failed to parse MiniMessage text: {}", text, ex);
+            return Component.literal(text);
         }
     }
 }
